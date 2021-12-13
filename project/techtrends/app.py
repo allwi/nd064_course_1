094298202ff1de5e50d7ed667924defa4296274a
Klaus Allwicher <92708832+allwicher@users.noreply.github.com>
2021-12-13 15:35:52 +1100

    Step3

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -1,21 +1,39 @@
 import sqlite3
+import logging
 
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
+
+logging.basicConfig(format='%(levelname)s: %(asctime)s - %(message)s', level=logging.DEBUG)
+
+class Counter:
+    num_open = 0
 
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
+    Counter.num_open += 1
     return connection
+
+
+def close_db_connection(connection):
+    connection.close()
+    Counter.num_open -= 1
+
+def get_num_posts():
+    connection = get_db_connection()
+    posts = connection.execute('SELECT * FROM posts').fetchall()
+    close_db_connection(connection)
+    return len(posts)
 
 # Function to get a post using its ID
 def get_post(post_id):
     connection = get_db_connection()
     post = connection.execute('SELECT * FROM posts WHERE id = ?',
                         (post_id,)).fetchone()
-    connection.close()
+    close_db_connection(connection)
     return post
 
 # Define the Flask application
@@ -27,7 +45,7 @@ app.config['SECRET_KEY'] = 'your secret key'
 def index():
     connection = get_db_connection()
     posts = connection.execute('SELECT * FROM posts').fetchall()
-    connection.close()
+    close_db_connection(connection)
     return render_template('index.html', posts=posts)
 
 # Define how each individual article is rendered 
@@ -36,13 +54,16 @@ def index():
 def post(post_id):
     post = get_post(post_id)
     if post is None:
-      return render_template('404.html'), 404
+        logging.warning('Failed to retrieve post with ID {}'.format(post_id))
+        return render_template('404.html'), 404
     else:
-      return render_template('post.html', post=post)
+        logging.info("Retrieved post '{}' ID {}".format(post['title'], post_id))
+        return render_template('post.html', post=post)
 
 # Define the About Us page
 @app.route('/about')
 def about():
+    logging.info("Retrieve 'About' page")
     return render_template('about.html')
 
 # Define the post creation functionality 
@@ -59,11 +80,30 @@ def create():
             connection.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
                          (title, content))
             connection.commit()
-            connection.close()
-
+            close_db_connection(connection)
+            logging.info("Created article '{}'".format(title))
             return redirect(url_for('index'))
-
     return render_template('create.html')
+
+
+@app.route('/healthz')
+def status():
+    response = app.response_class(
+            response=json.dumps({"result":"OK - healthy"}),
+            status=200,
+            mimetype='application/json'
+    )
+    return response
+
+@app.route('/metrics')
+def metrics():
+    num_posts = get_num_posts()
+    response = app.response_class(
+            response=json.dumps({"db_connection_count": Counter.num_open, "post_count": num_posts}),
+            status=200,
+            mimetype='application/json'
+    )
+    return response
 
 # start the application on port 3111
 if __name__ == "__main__":
